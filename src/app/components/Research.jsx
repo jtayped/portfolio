cbc564123e8b138241843a1fcd11e298dd3e1734
Joel Taylor <jtayped@gmail.com>
2024-01-27 22:32:24 +0100

    structure/image

--- src/app/components/Research.jsx
+++ src/app/components/Research.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { BsFillFileTextFill } from "react-icons/bs";
+import SEOImage from "@/../public/research/seo.jpg";
 
 const Research = () => {
   return (
     <div className="text-black px-8 py-10 flex items-center justify-center">
       <div className="w-[900px]">
-        <div className="flex flex-col gap-10"></div>
+        <div className="flex flex-col md:grid md:grid-cols-2 items-center gap-8">
+          <div>
+            <Image
+              src={SEOImage}
+              width={400}
+              height={200}
+              className="shadow rounded"
+            ></Image>
+          </div>
+          <div>
+            <h2 className="text-3xl font-bold">
+              Check out my Research Project!
+            </h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Accusamus mollitia facilis assumenda earum voluptatibus porro ad
+              repellendus illo voluptates aspernatur. Sed eveniet perferendis
+              excepturi quae quidem. Fuga earum iusto minima.
+            </p>
+            <Link
+              href="/research"
+              className="flex items-center gap-2 bg-black text-white w-fit px-2 py-1 rounded mt-2 text-sm"
+            >
+              <BsFillFileTextFill />
+              Learn more
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
