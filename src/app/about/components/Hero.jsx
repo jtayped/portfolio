5d3afb8ede2cac6753dfbb25e91e42b781a30608
Joel Taylor <jtayped@gmail.com>
2024-01-28 17:32:55 +0100

    wording

--- src/app/about/components/Hero.jsx
+++ src/app/about/components/Hero.jsx
@@ -14,8 +14,8 @@ const Hero = () => {
           <div className="max-w-[400px] text-center sm:text-left">
             <h1 className="text-6xl font-bold">About Me</h1>
             <p>
-              I'm Joel Taylor, a 17-year-old from Catalonia, Spain, navigating
-              Batxillerat with a passion for Computer Science. Fluent in
+              I'm Joel Taylor, a 17-year-old from Catalonia, Spain, studying
+              a Baccalaureate with a passion for Computer Science. Fluent in
               Spanish, Catalan, and English, I'm driven by curiosity and
               immersed in coding.
             </p>
