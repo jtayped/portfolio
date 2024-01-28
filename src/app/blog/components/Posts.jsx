8180a33a4df3eab6c9c324c4bfc3e3897b8ecf9d
Joel Taylor <jtayped@gmail.com>
2024-01-28 18:16:58 +0100

    post ordering/responsiveness

--- src/app/blog/components/Posts.jsx
+++ src/app/blog/components/Posts.jsx
@@ -2,26 +2,15 @@ import React from "react";
 import { PostPreview } from "./previews";
 
 const Posts = ({ posts }) => {
-  const numColumns = 3;
-  // Create a new array with the desired order of posts
-  const orderedPosts = posts.reduce((acc, post, index) => {
-    const columnIndex = index % numColumns;
-    acc[columnIndex] = [...(acc[columnIndex] || []), post];
-    return acc;
-  }, []);
-
-  // Create an array of columns, each containing a slice of ordered posts
-  const columns = orderedPosts.map((columnPosts, columnIndex) => (
-    <div key={columnIndex} className="flex flex-col gap-4">
-      {columnPosts.map((post, i) => (
+  return (
+    <ol className="grid grid-cols-1 min-[450px]:grid-cols-2 md:grid-cols-3 gap-3">
+      {posts.map((post, i) => (
         <li key={i}>
           <PostPreview post={post} />
         </li>
       ))}
-    </div>
-  ));
-
-  return <ol className="grid grid-cols-3 gap-3">{columns}</ol>;
+    </ol>
+  );
 };
 
 export default Posts;
