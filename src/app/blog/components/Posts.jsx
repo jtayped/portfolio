79cd72f91f4ad1c0f0d35e2a9d431ad31e150388
Joel Taylor <jtayped@gmail.com>
2024-01-28 18:47:41 +0100

    layout responsiveness

--- src/app/blog/components/Posts.jsx
+++ src/app/blog/components/Posts.jsx
@@ -1,14 +1,54 @@
 import React from "react";
 import { PostPreview } from "./previews";
 
+const generateOrderedPosts = (posts, numColumns) => {
+  return posts.reduce((acc, post, index) => {
+    const columnIndex = index % numColumns;
+    acc[columnIndex] = [...(acc[columnIndex] || []), post];
+    return acc;
+  }, []);
+};
+
 const Posts = ({ posts }) => {
+  // Define the number of columns for large screens
+  const numColumnsLarge = 3;
+
+  // Create a new array with the desired order of posts for large screens
+  const orderedPostsLarge = generateOrderedPosts(posts, numColumnsLarge);
+
+  // Define the number of columns for medium screens
+  const numColumnsMedium = 2;
+
+  // Create a new array with the desired order of posts for medium screens
+  const orderedPostsMedium = generateOrderedPosts(posts, numColumnsMedium);
+
   return (
-    <ol className="grid grid-cols-1 min-[450px]:grid-cols-2 md:grid-cols-3 gap-3">
-      {posts.map((post, i) => (
-        <li key={i}>
-          <PostPreview post={post} />
-        </li>
-      ))}
+    <ol className="">
+      {/* For medium screens, display 2 columns */}
+      <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2 md:hidden">
+        {orderedPostsMedium.map((columnPosts, columnIndex) => (
+          <div key={columnIndex} className="flex flex-col gap-4">
+            {columnPosts.map((post, i) => (
+              <li key={i}>
+                <PostPreview post={post} />
+              </li>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      {/* For large screens, display 3 columns */}
+      <div className="hidden md:grid md:grid-cols-3 gap-4">
+        {orderedPostsLarge.map((columnPosts, columnIndex) => (
+          <div key={columnIndex} className="flex flex-col gap-4">
+            {columnPosts.map((post, i) => (
+              <li key={i}>
+                <PostPreview post={post} />
+              </li>
+            ))}
+          </div>
+        ))}
+      </div>
     </ol>
   );
 };
