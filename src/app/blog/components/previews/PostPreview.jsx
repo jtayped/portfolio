cbec9ff6829d8f9d07fc3f9572a6dd75cf55969b
Joel Taylor <jtayped@gmail.com>
2024-01-28 18:10:30 +0100

    author added

--- src/app/blog/components/previews/PostPreview.jsx
+++ src/app/blog/components/previews/PostPreview.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Me from "@/../../public/me.jpg";
 
 const PostPreview = ({ post }) => {
   return (
     <div className="bg-black text-white flex flex-col justify-between h-full p-4 rounded-lg">
       <div>
-        {" "}
         <Image
           src={post.cover}
           width={300}
           height={200}
           className="object-cover w-full h-[150px] rounded-sm"
         ></Image>
+        <div className="flex items-center gap-3 mt-2">
+          <Image
+            src={Me}
+            width={35}
+            height={35}
+            className="rounded-full"
+          ></Image>
+          <div className="flex flex-col text-xs">
+            <p className="font-bold">Joel Taylor</p>
+            <time>{post.date}</time>
+          </div>
+        </div>
         <h2 className="text-lg font-bold mt-2">{post.title}</h2>
         <p className="text-sm">{post.description}</p>
       </div>
