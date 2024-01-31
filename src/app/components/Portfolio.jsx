64870fcb3c95b75b9d5c10576c4aeac81a7f26f5
Joel Taylor <jtayped@gmail.com>
2024-01-31 19:02:31 +0100

    unused imports

--- src/app/components/Portfolio.jsx
+++ src/app/components/Portfolio.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { work } from "@/constants/portfolio";
-import PortfolioPiece from "./PortfolioPiece";
 import { BsGithub } from "react-icons/bs";
 import { MockupViewer } from ".";
 
