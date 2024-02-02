d649a3232689d9ca0e8fcaf4b455d6e2139939ed
Joel Taylor <jtayped@gmail.com>
2024-02-02 19:38:43 +0100

    navbar translation

--- src/app/[locale]/components/Navbar.jsx
+++ src/app/[locale]/components/Navbar.jsx
@@ -1,25 +1,29 @@
-import {Link} from "@/navigation";
+import { Link } from "@/navigation";
 import { pages } from "@/constants/pages";
 import { Menu } from ".";
+import { useTranslations } from "next-intl";
 
 const Navbar = () => {
+  const t = useTranslations("Header");
+  const keys = ["about", "portfolio", "blog", "contact"];
+
   return (
     <header className="fixed top-0 left-0 w-full flex items-center justify-between py-8 sm:py-10 px-8 md:px-20 text-black z-10 bg-yellow/80 backdrop-blur-sm">
       <Link href="/">
         <p className="text-2xl sm:text-4xl font-extrabold">TAYLOR</p>
       </Link>
       <nav className="hidden md:flex items-center gap-10 text-lg uppercase">
-        {pages.map((page, i) => (
+        {keys.map((key, i) => (
           <Link
-            href={page.path}
+            href={t(`${key}.path`)}
             key={i}
             className={`${
-              page.accent
+              t(key + ".accent")
                 ? "bg-black text-yellow hover:bg-black/90 transition-colors px-3 py-1 rounded-sm shadow"
                 : "tracking-wider"
             }`}
           >
-            {page.name}
+            {t(`${key}.name`)}
           </Link>
         ))}
       </nav>
