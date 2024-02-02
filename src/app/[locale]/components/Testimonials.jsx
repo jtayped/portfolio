635b5627474bb15e11aa17039a80972d22f933e6
Joel Taylor <jtayped@gmail.com>
2024-02-02 17:44:14 +0100

    translation

--- src/app/[locale]/components/Testimonials.jsx
+++ src/app/[locale]/components/Testimonials.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import { testimonials } from "@/constants/testimonials";
 import { BsFillStarFill } from "react-icons/bs";
 import { useTranslations } from "next-intl";
+import { BsTranslate } from "react-icons/bs";
 
 const Testimonials = () => {
   const t = useTranslations("Index.testimonials");
   const keys = ["Juan", "Vithanco", "Walter"];
-
   return (
     <div className="text-black px-8 py-10 flex items-center justify-center">
       <div className="w-[900px]">
@@ -17,22 +16,31 @@ const Testimonials = () => {
             {keys.map((key) => (
               <li key={key}>
                 <div className="bg-white/80 text-black p-4 rounded-lg shadow">
-                  <ul className="flex items-start gap-1">
-                    {Array.from({ length: 5 }).map((_, index) => (
-                      <li
-                        key={index}
-                        viewport={{ once: true }}
-                        initial={{ rotate: 90, y: 5, opacity: 0 }}
-                        whileInView={{ rotate: 0, y: 0, opacity: 1 }}
-                        transition={{ delay: index * 0.05, duration: 0.15 }}
-                      >
-                        <BsFillStarFill className="text-yellow" />
-                      </li>
-                    ))}
-                  </ul>
-                  <p className="text-xl font-bold mt-2">
+                  <div className="flex justify-between">
+                    <ul className="flex items-start gap-1">
+                      {Array.from({ length: 5 }).map((_, index) => (
+                        <li
+                          key={index}
+                          viewport={{ once: true }}
+                          initial={{ rotate: 90, y: 5, opacity: 0 }}
+                          whileInView={{ rotate: 0, y: 0, opacity: 1 }}
+                          transition={{ delay: index * 0.05, duration: 0.15 }}
+                        >
+                          <BsFillStarFill className="text-yellow" />
+                        </li>
+                      ))}
+                    </ul>
+                    {t(`testimonials.${key}.translated`) ? (
+                      <BsTranslate
+                        className="text-black/80"
+                        aria-label="Translated"
+                      />
+                    ) : null}
+                  </div>
+
+                  <h3 className="text-xl font-bold mt-2">
                     {t(`testimonials.${key}.title`)}
-                  </p>
+                  </h3>
                   <p className="text-sm">"{t(`testimonials.${key}.text`)}"</p>
                   <div className="flex items-center gap-2 mt-3">
                     <Image
