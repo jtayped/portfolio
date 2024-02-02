2703cf757b55bfab18f79b9fb8575de1502c7bdf
Joel Taylor <jtayped@gmail.com>
2024-02-02 20:13:38 +0100

    timeline translation

--- src/app/[locale]/about/components/Timeline.jsx
+++ src/app/[locale]/about/components/Timeline.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { events } from "@/constants/timeline";
 import Image from "next/image";
 import { Link } from "@/navigation";
+import { useTranslations } from "next-intl";
+
+const keys = [
+  "firstComputer",
+  "fllTenerife",
+  "covid",
+  "startPython",
+  "fllTenerifeOnline",
+  "fllTorremolinos",
+  "startWebDev",
+  "startFiverr",
+];
 
 const months = [
   "January",
@@ -38,6 +50,7 @@ const currentMonthYear = `${
 } ${new Date().getFullYear()}`;
 
 const Timeline = () => {
+  const t = useTranslations("About.myStory");
   return (
     <div className="text-black px-8 py-10 flex items-center justify-center">
       <div className="w-[900px] flex flex-col items-center gap-10">
@@ -45,26 +58,28 @@ const Timeline = () => {
           <h2 className="text-3xl font-bold">My Story</h2>
         </div>
         <ol class="relative border-s border-black max-w-[600px]">
-          {events.map((event, i) => (
+          {keys.map((key, i) => (
             <li key={i} class="mb-10 ms-4">
               <div class="absolute w-3 h-3 bg-black rounded-full mt-1.5 -start-1.5"></div>
               <div>
                 <div>
                   <div className="flex items-center mb-1 text-sm leading-none text-black/80 gap-2">
-                    <time>{event.year}</time>
-                    <p className="font-bold">({event.age} y.o.)</p>
+                    <time>{t(`events.${key}.year`)}</time>
+                    <p className="font-bold">({t(`events.${key}.age`)} y.o.)</p>
                   </div>
 
-                  <h3 class="text-lg font-semibold">{event.name}</h3>
-                  <p class="mb-4 text-base">{event.description}</p>
+                  <h3 class="text-lg font-semibold">
+                    {t(`events.${key}.name`)}
+                  </h3>
+                  <p class="mb-4 text-base">{t(`events.${key}.description`)}</p>
                 </div>
-                {event.image ? (
+                {t(`events.${key}.image`) ? (
                   <Image
-                    src={event.image}
+                    src={t(`events.${key}.image`)}
                     height={200}
                     width={400}
                     className="h-[160px] w-[350px] object-cover rounded"
-                    alt={event.name}
+                    alt={t(`events.${key}.name`)}
                   ></Image>
                 ) : null}
               </div>
@@ -79,25 +94,14 @@ const Timeline = () => {
                   <p className="font-bold">({age} y.o.)</p>
                 </div>
 
-                <h3 class="text-lg font-semibold">Today</h3>
-                <p class="text-base mb-1">
-                  Today, I stand as a passionate web developer, ready to turn
-                  your digital dreams into reality. From building my first
-                  computer at 13 to thriving as a freelancer on Fiverr, my
-                  journey embodies a commitment to innovation and continuous
-                  learning.
-                </p>
-                <p className="text-base mb-4">
-                  Let's collaborate, innovate, and bring your digital
-                  aspirations to life. Today is not just another day; it's an
-                  opportunity to create something extraordinary. Let's make it
-                  happen together.
-                </p>
+                <h3 class="text-lg font-semibold">{t("today.title")}</h3>
+                <p class="text-base mb-1">{t("today.description.p1")}</p>
+                <p className="text-base mb-4">{t("today.description.p2")}</p>
                 <Link
                   href="/#contact"
                   className="p-2 px-4 rounded bg-black text-white hover:bg-black/90 text-sm"
                 >
-                  TAKE ACTION!
+                  {t("today.button")}
                 </Link>
               </div>
             </div>
