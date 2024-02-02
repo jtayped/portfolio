5d5b8892850f0a3bd60bedeff370b808f26709ce
Joel Taylor <jtayped@gmail.com>
2024-02-02 19:24:28 +0100

    index blog content

--- src/app/[locale]/blog/page.jsx
+++ src/app/[locale]/blog/page.jsx
@@ -1,16 +1,25 @@
 import { Posts_main } from "./components";
 import { useTranslations } from "next-intl";
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "@/navigation";
 
 const Page = () => {
   const t = useTranslations("Blog.hero");
 
   return (
-    <div className="relative text-black px-8 py-10 flex items-center justify-center mt-[100px] sm:mt-[200px]">
+    <div className="relative text-black px-8 py-10 flex items-center justify-center mt-[100px]">
       <main className="max-w-[900px] flex items-center justify-center">
         <div>
-          <div className="text-center">
+          <div className="max-w-[600px] flex flex-col gap-3">
             <h1 className="text-4xl sm:text-6xl font-bold">{t("title")}</h1>
-            <p className="mt-2">{t("description")}</p>
+            <p>{t("description")}</p>
+            <Link
+              className="bg-black text-white px-3 py-2 rounded w-fit hover:bg-black/90 flex items-center gap-2 text-sm"
+              href="/"
+            >
+              {t("button")}
+              <FaArrowRight />
+            </Link>
           </div>
           <Posts_main />
         </div>
