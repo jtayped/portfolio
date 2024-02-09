0f3069d2705084323523ea725a1385552b26526b
Joel Taylor <jtayped@gmail.com>
2024-02-09 23:50:38 +0100

    contact functionality

--- src/app/[locale]/components/ContactForm.jsx
+++ src/app/[locale]/components/ContactForm.jsx
@@ -1,14 +1,24 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { FaArrowRight } from "react-icons/fa6";
+import axios from "axios";
+import { LuCheck } from "react-icons/lu";
 
-const ContactInput = ({ placeholder, type, autocomplete }) => {
+const sendEmail = async (name, lastName, email, subject, body) => {
+  await axios.post(
+    "/api/send",
+    JSON.stringify({ name, lastName, email, subject, body })
+  );
+};
+
+const ContactInput = ({ placeholder, type, autocomplete, setValue }) => {
   return (
     <div className="border border-black rounded p-2">
       <input
         placeholder={placeholder}
         type={type}
         autoComplete={autocomplete}
+        onChange={(e) => setValue(e.target.value)}
         className="w-full bg-transparent outline-none placeholder:text-black placeholder:text-sm"
       />
     </div>
@@ -16,8 +26,28 @@ const ContactInput = ({ placeholder, type, autocomplete }) => {
 };
 
 const ContactForm = ({ name, lastName, email, subject, message, sendBtn }) => {
-  function handleSubmit(e) {
+  const [nameValue, setName] = useState("");
+  const [lastNameValue, setLastName] = useState("");
+  const [emailValue, setEmail] = useState("");
+  const [subjectValue, setSubject] = useState("");
+  const [bodyValue, setBody] = useState("");
+
+  const [state, setState] = useState("");
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setState("loading");
+
+    console.log(emailValue);
+    await sendEmail(
+      nameValue,
+      lastNameValue,
+      emailValue,
+      subjectValue,
+      bodyValue
+    );
+
+    setState("success");
   }
 
   return (
@@ -30,15 +60,27 @@ const ContactForm = ({ name, lastName, email, subject, message, sendBtn }) => {
           placeholder={name}
           type="text"
           autocomplete="given-name"
+          setValue={setName}
         />
         <ContactInput
           placeholder={lastName}
           type="text"
           autocomplete="family-name"
+          setValue={setLastName}
         />
       </div>
-      <ContactInput placeholder={email} type="email" autocomplete="email" />
-      <ContactInput placeholder={subject} type="text" autocomplete="off" />
+      <ContactInput
+        placeholder={email}
+        type="email"
+        autocomplete="email"
+        setValue={setEmail}
+      />
+      <ContactInput
+        placeholder={subject}
+        type="text"
+        autocomplete="off"
+        setValue={setSubject}
+      />
       <div className="border border-black p-2 rounded">
         <textarea
           className="w-full bg-transparent outline-none placeholder:text-black placeholder:text-sm"
@@ -47,6 +89,7 @@ const ContactForm = ({ name, lastName, email, subject, message, sendBtn }) => {
           cols="30"
           rows="8"
           autoComplete="off"
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
       </div>
       <div className="grid sm:grid-cols-2 gap-2">
@@ -55,8 +98,36 @@ const ContactForm = ({ name, lastName, email, subject, message, sendBtn }) => {
           type="submit"
           className="bg-black hover:bg-black/95 py-2 text-white text-sm rounded font-bold flex justify-center items-center gap-2"
         >
-          {sendBtn}
-          <FaArrowRight />
+          {state ? (
+            state === "loading" ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  class="w-5 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-yellow"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            ) : (
+              <LuCheck className="text-green-500" size={20} />
+            )
+          ) : (
+            <>
+              {sendBtn}
+              <FaArrowRight />
+            </>
+          )}
         </button>
       </div>
     </form>
